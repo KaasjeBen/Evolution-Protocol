3f7496e67303fa875643793ddf6426ee7e84dbb4
Ben <D310581@edu.curio.nl>
2026-09-23 20:18:38 +0200

    Script error fix

--- Assets/Sprites/Scripts/PlayerController2D.cs
+++ Assets/Sprites/Scripts/PlayerController2D.cs
@@ -9,6 +9,15 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 movement;
 
+    private void Awake()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+
+        if (anim == null)
+            anim = GetComponentInChildren<Animator>();
+    }
+
     // Called by the new Input System
     public void OnMove(InputValue value)
     {
@@ -17,12 +26,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (rb != null)
+        if (rb == null)
+            return;
+
+        if (anim != null)
         {
             anim.SetFloat("horizontal", Mathf.Abs(movement.x));
             anim.SetFloat("vertical", Mathf.Abs(movement.y));
-
-            rb.linearVelocity = movement * speed;
         }
+
+        rb.linearVelocity = movement * speed;
     }
 }
