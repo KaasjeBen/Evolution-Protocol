5618b2621b390145a4bea5afab11b89a10ae7cb8
lucassss08_nl <lucaskoolen08@gmail.com>
2026-09-24 10:54:12 +0200

    player flipping
    de player kan nu ook naar links kijken als je naar lings loopt 'flipt' de player in de X asis

--- Assets/Sprites/Scripts/PlayerController2D.cs
+++ Assets/Sprites/Scripts/PlayerController2D.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public int facingDirection = 1; // 1 for right, -1 for left
+
     public Rigidbody2D rb;
     public Animator anim;
 
@@ -17,12 +19,36 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         if (rb != null)
+
+           
         {
             anim.SetFloat("horizontal", Mathf.Abs(movement.x));
             anim.SetFloat("vertical", Mathf.Abs(movement.y));
 
+            if (horizontal > 0 && transform.localScale.x < 0 ||
+                (horizontal < 0 && transform.localScale.x > 0))
+            {
+               Flip ();
+            }
+
             rb.linearVelocity = movement * speed;
         }
     }
+
+    
+    void Flip()
+    {
+        facingDirection *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 }
+
+
+    
